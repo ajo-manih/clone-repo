76d521decc8a6c78344b03ca5ed930d078fdfa53
ajo-manih <judachaniago@gmail.com>
2025-05-22 12:42:12 +0000

    tugas fix

--- tugas2.java
+++ tugas2.java
@@ -3,19 +3,14 @@ public class Program2 {
         // Tipe data boolean
         boolean b = true;
         System.out.println(b); 
-
         boolean c = false;
         System.out.println(c); 
-
         int a = 3 + 4;
         System.out.println(a); 
-
         double d = Math.random();
         System.out.println(d); 
-
         char e = 'd' + 1;
         System.out.println(e); 
-        
         String h = "Hello!";
         System.out.println(h); 
     }
--- tugas3.java
+++ tugas3.java
@@ -4,17 +4,14 @@ public class program3 {
         d = 3.14;
         d = d + 3.12;
         System.out.println(d);
-
         System.out.println("penambahan 3+5 = " + (3 + 5));
         System.out.println("pengurangan 5-3 = " + (5 - 3));
         System.out.println("perkalian 5*5 = " + (5 * 5));
         System.out.println("pembagian 90/5 = " + (90 / 5));
         System.out.println("modulus 52%6 = " + (52 % 6));
-
         String h;
         h = "Wow" + "Whoo";
         System.out.println(h);
-
         boolean i = false;
         System.out.println(i == false);
         boolean j = false;
@@ -22,7 +19,6 @@ public class program3 {
         System.out.println(j && k);
         System.out.println(j || k);
         System.out.println(!j);
-
         int g = 5;
         System.out.println(3 < g);
     }
