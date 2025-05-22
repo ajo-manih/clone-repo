e0edc2a6b951ba39c3fc8513a11832bb4c9170b6
ajo-manih <judachaniago@gmail.com>
2025-05-22 12:37:01 +0000

    tugas

---
+++ tugas3.java
@@ -0,0 +1,29 @@
+public class program3 {
+    public static void main(String[] args) {
+        double d;
+        d = 3.14;
+        d = d + 3.12;
+        System.out.println(d);
+
+        System.out.println("penambahan 3+5 = " + (3 + 5));
+        System.out.println("pengurangan 5-3 = " + (5 - 3));
+        System.out.println("perkalian 5*5 = " + (5 * 5));
+        System.out.println("pembagian 90/5 = " + (90 / 5));
+        System.out.println("modulus 52%6 = " + (52 % 6));
+
+        String h;
+        h = "Wow" + "Whoo";
+        System.out.println(h);
+
+        boolean i = false;
+        System.out.println(i == false);
+        boolean j = false;
+        boolean k = true;
+        System.out.println(j && k);
+        System.out.println(j || k);
+        System.out.println(!j);
+
+        int g = 5;
+        System.out.println(3 < g);
+    }
+}
---
+++ tugas4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class program4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan nilai: ");
+        int x = input.nextInt();
+        System.out.println("Nilai = " + x);
+    }
+}
